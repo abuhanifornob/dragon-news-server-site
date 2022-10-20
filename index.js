e4d77fbefdd62c2221d0589dbd2c05d8443d8b50
Abu Hanif Arnob <hanifcse90@gmail.com>
2022-10-20 06:22:52 +0600

    Some api create

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-const express=require('express')
-const app=express();
-const port=process.env.PORT || 5000;
- 
-const category=require("./data/category.json");
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+app.use(cors())
 
-app.get("/",(req,res)=>{
+
+const category = require("./data/category.json");
+const news = require("./data/news.json");
+
+app.get("/", (req, res) => {
     res.send("News API Runing")
-})
-app.get("/news-category",(req,res)=>{
-    res.send(category);
-}) 
+});
+app.get("/news-category", (req, res) => {
+    res.send(category)
+});
+app.get("/category/:id", (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news)
+    } else {
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews);
+    }
 
-app.listen(port,()=>{
-    console.log("Dragon surver running",port);
+})
+app.get("/news/:id", (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+});
+
+app.listen(port, () => {
+    console.log("Dragon surver running", port);
 })
